e03b8f1dd67bf97cc88628e77f488ea4198c4378
hansanguk0222 <hansanguk0222@gmail.com>
2020-02-22 16:41:31 +0900

    크롤링 프로미스로 수정

--- crawling.js
+++ crawling.js
@@ -1,22 +1,26 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const sanitizeHtml = require('sanitize-html');
+const rp = require('request-promise');
 
-const options = {
-    url : 'http://www.kmou.ac.kr/kmou/main.do',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;1=0.9*/*;q=0.8',
-        'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        'Accept-Encoding' : null,
-        'Accept-Language' : 'en-US,en;q=0.8',
-        'Connection' : 'keep-alive'
-    }
+const crawling_data = (url_str) => {
+    return new Promise(async (resolve, reject) => {
+        const options = await {
+            url : url_str,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;1=0.9*/*;q=0.8',
+                'Accept-Charset' : 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                'Accept-Encoding' : null,
+                'Accept-Language' : 'en-US,en;q=0.8',
+                'Connection' : 'keep-alive'
+            }
+        }
+
+        const source = await rp(options)
+        .then((result) => {
+            resolve(result);
+        })
+
+        resolve(source);
+    }) 
 }
 
-request(options, (error, response, html) => {
-    if(error) throw error;
-    console.log(html);
-});
-
-
+exports.crawling_data = crawling_data
